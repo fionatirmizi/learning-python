548296d9a9600ffac3501975ff17d07d0a305721
fionatirmizi <84565455+fionatirmizi@users.noreply.github.com>
2021-05-21 12:42:22 +0400

    Update ex1.py
    Last working code. 21/5

--- ex1.py
+++ ex1.py
@@ -1,1 +1,9 @@
-print('Lesson 1')
+print('Hello, world!')
+print('Hello Fiona!')
+# This is a comment. Lesson 1. 
+mybirthday = "3rd july"
+name = "Fiona"
+print(name+"'s birthday is on "+ mybirthday)
+dog = "Newton"
+sentence = name + "'s dog is " + dog
+print(sentence)
