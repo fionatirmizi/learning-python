e331f0b24afb7b726d09e95012f59727ece8bb03
fionatirmizi <84565455+fionatirmizi@users.noreply.github.com>
2021-05-21 13:00:23 +0400

    Update ex1.py
    printing name multiple times, can edit name or count later

--- ex1.py
+++ ex1.py
@@ -7,3 +7,18 @@ print(name+"'s birthday is on "+ mybirthday)
 dog = "Newton"
 sentence = name + "'s dog is " + dog
 print(sentence)
+
+
+a = 5
+b = 10
+print( a + b)
+print(5 + 10)
+print('fiona', 'fiona')
+name = "fiona"
+print(name * 5)
+# making more efficient
+print('fiona '  * 5)
+count = 10
+# printing name multiple times, can edit name or count later
+print( name , count)
+print( (name + " ")  * count)
