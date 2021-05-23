3f6be0626f4d47d210f23bcfaa272a20e9c3b2e6
fionatirmizi <84565455+fionatirmizi@users.noreply.github.com>
2021-05-23 14:56:20 +0400

    Update ex2.py
    Using more escape sequences

--- ex2.py
+++ ex2.py
@@ -8,3 +8,24 @@ print (formatter % ('''My name is Fiona''', '''I have a dog''',
 formatter2 = '%s %s %s %s'
 print (formatter2 % ('I am on a mission\n', 'To become a good coder\n',
 'it could take a while\n', 'but i will be consistent.'))
+
+
+# Using more escape sequences
+#using " to show inches, but not wanting to end the string! 
+print ("I am 5'2\" tall")
+
+#ex10 from book
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+# starts on a new line every line because I used """ , and didnt need 
+#didnt need \n on every line 
+fat_cat = """
+I'll do a list:
+\t* cat food 
+\t* fishies
+\t* catnip\n\t* grass """
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
